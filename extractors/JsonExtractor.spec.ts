e2b343082aa7ba7000aee23cd4fbf1583c33f496
Christoph Bühler <christoph@smartive.ch>
2016-03-23 00:14:39 +0100

    adding json extractor code and tests

---
+++ extractors/JsonExtractor.spec.ts
@@ -0,0 +1,29 @@
+import chai = require('chai');
+import asPromised = require('chai-as-promised');
+import {JsonExtractor} from './JsonExtractor';
+
+let should = chai.should();
+chai.use(asPromised);
+
+describe('JsonExtractor', () => {
+
+    it('should get correct path', () => {
+        let ext = new JsonExtractor('hello');
+        let anyExt:any = ext;
+        anyExt.path.should.equal('hello');
+    });
+
+    it('should receive a json object', () => {
+        let ext = new JsonExtractor('../../.testdata/json-extractor.object.json');
+        return ext.read().should.eventually.deep.equal({
+            "foo": "bar",
+            "hello": "world"
+        });
+    });
+
+    it('should throw on not found file', () => {
+        let ext = new JsonExtractor('404.json');
+        return ext.read().should.be.rejected;
+    });
+
+});
--- extractors/JsonExtractor.ts
+++ extractors/JsonExtractor.ts
@@ -2,11 +2,15 @@ import {IExtract} from '../interfaces/IExtract';
 import {Promise} from 'es6-promise';
 
 export class JsonExtractor implements IExtract {
-
-    constructor(private path) {
+    constructor(private path:string) {
     }
 
     public read():Promise<any> {
-        return null;
+        try {
+            let file = require(this.path);
+            return Promise.resolve(file);
+        } catch (e) {
+            return Promise.reject(e);
+        }
     }
 }
