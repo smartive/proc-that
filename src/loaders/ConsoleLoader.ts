431938b4ff7760c6a5b30ab93df2ccd8ce13ff7a
Christoph Bühler <christoph@smartive.ch>
2017-11-06 10:51:30 +0100

    add docs

--- src/loaders/ConsoleLoader.ts
+++ src/loaders/ConsoleLoader.ts
@@ -2,6 +2,13 @@ import { Observable } from 'rxjs';
 
 import { Loader } from '../interfaces/Loader';
 
+/**
+ * Loader that outputs everything to the console.
+ *
+ * @export
+ * @class ConsoleLoader
+ * @implements {Loader}
+ */
 export class ConsoleLoader implements Loader {
     public write(object: any): Observable<any> {
         console.log(object);
